3bd33795aab67ff3dbfa9a5e925318a05a9160e4
Felipe Velásquez <Felipe.lfvv.16@gmail.com>
2024-03-24 09:43:49 -0600

    Add project files.

---
+++ parte9_Guia_HttpHelper/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace parte9_Guia_HttpHelper.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
